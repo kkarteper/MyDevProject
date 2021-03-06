18f813b2547e12018451c5f21f8d9bc7692b9987
kkarteper <kkarteper@yahoo.com>
2021-03-06 11:25:21 -0500

    I am a developer right now thus is my first commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Hello batch 8");
+            Thread.sleep(2000);
+        }
+
+
+
+    }
+}
